91f3d93628566d23e417fc9f17620aba92a59603
Daniel van Droffelaar <daniel@realafford.nl>
2021-03-31 09:56:30 +0200

    Cleaned up the code and added comments

--- Templucht/Templucht.ino
+++ Templucht/Templucht.ino
@@ -6,6 +6,8 @@ dht11 DHT11;
 #include <hd44780.h>                      
 #include <hd44780ioClass/hd44780_I2Cexp.h> 
 hd44780_I2Cexp lcd; 
+
+//VARIABLES
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
 const char start[] = "Welcome back!";
@@ -19,13 +21,16 @@ String userhum = " ";
 String degree= " ";
 char object = ' ';
 bool otherScreen = true;
+bool skipDelay = false;
 
-
+//SETUP
 void setup()
 {
   Serial.begin(115200);
   lcd.begin(LCD_COLS, LCD_ROWS);
   lcd.lineWrap();
+
+  //print welcome message letter for letter
   for(int i = 0; i < strlen(start); i++){
     char c = start[i];
     lcd.print(c);
@@ -34,48 +39,55 @@ void setup()
   delay(2000);
 }
 
+
+//LOOP
 void loop()
 {
+  //read the new values of temperature and humidity
   int chk = DHT11.read(DHT11PIN);
   int newtemp = DHT11.temperature;
   int newhum = DHT11.humidity;
-  
+
+  //check and print if new values is NOT the same as old values
   if(newtemp != oldtemp || newhum != oldhum || otherScreen == true){
+    //put values on lcd screen
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Temperature: ");
-    lcd.print(newtemp);
-    lcd.print((char)223);
-    lcd.print("C");
+    lcd.print("Temperature: " + String(newtemp) + (char)223 + "c");
     lcd.setCursor(0, 1);
-    lcd.print("Humidity: ");
-    lcd.print(newhum);
-    lcd.print("%");  
+    lcd.print("Humidity: " + String(newhum) + "%");
     oldtemp = DHT11.temperature, 0;
     oldhum = DHT11.humidity;
     otherScreen == false;  
   }
   
 
+  //every 60 send temperature/humidity values to pi 
   if(timeToSend == 12){
      String message = String(newtemp) + " " +  String(newhum);
      Serial.println(message);
      timeToSend = 0;
   }
 
-
+  //reads the data send from the raspberry pi
   data = Serial.read();  
   updateUserPref(data);
   
 
   timePassed = timePassed + 1;
   timeToSend = timeToSend + 1;
-  delay(5000);
-  
+
+  //if temp/hum shows on screen next repeat no delay
+  if(skipDelay){
+    delay(100);
+    skipDelay = false;
+  } else {
+    delay(5000);
+  }
 }
 
 
-
+//determine the code if temp/hum is high or low based on user preferences
 void updateUserPref(int data){
   switch (data) {
     case 't':
@@ -86,27 +98,27 @@ void updateUserPref(int data){
       break;
     case '5':
       degree = "too high";
-      printToScreen(degree, object);
+      setVariablesValues(degree, object);
       break;
     case '4':
       degree = "high";
-      printToScreen(degree, object);
+      setVariablesValues(degree, object);
       break;
     case '3':
       degree = "good";
-      printToScreen(degree, object);
+      setVariablesValues(degree, object);
       break;
     case '2':
       degree = "low";
-      printToScreen(degree, object);
+      setVariablesValues(degree, object);
       break;
     case '1':
       degree = "too low";
-      printToScreen(degree, object);
+      setVariablesValues(degree, object);
       break;
   }
 
-
+  //if code is made, show it every 20 seconds on the screen
   if(usertemp != " "){
     if(userhum != " "){
       if(timePassed == 4){
@@ -122,7 +134,8 @@ void updateUserPref(int data){
         lcd.setCursor(0, 1);
         lcd.print(userhum);
         timePassed = 0;
-        otherScreen == true;
+        otherScreen = true;
+        skipDelay = true;
         delay(100);
       } 
     }
@@ -130,7 +143,8 @@ void updateUserPref(int data){
 }
 
 
-void printToScreen(String degree, char object){
+//sets the variables for the code
+void setVariablesValues(String degree, char object){
   switch (object){
     case 't':
       usertemp = degree;
